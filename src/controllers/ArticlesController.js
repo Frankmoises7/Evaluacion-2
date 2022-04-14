211406c7fac1fcd853cc25f7d6da2028bae563e3
Frankmoises7 <frank.islas.zuniga@ciisa.cl>
2022-04-14 15:33:05 -0400

    First commit

---
+++ src/controllers/ArticlesController.js
@@ -0,0 +1,137 @@
+const ArticlesDAO = require('../models/dao/ArticlesDAO')
+
+class ArticlesController {
+  constructor (db) {
+    this.articlesDao = new ArticlesDAO(db)
+    this.renderHome = this.renderHome.bind(this)
+    this.renderSingleArticle = this.renderSingleArticle.bind(this)
+    this.renderArticleCreationForm = this.renderArticleCreationForm.bind(this)
+    this.renderArticleUpdateForm = this.renderArticleUpdateForm.bind(this)
+    this.insertAndRenderArticle = this.insertAndRenderArticle.bind(this)
+    this.updateAndRenderArticle = this.updateAndRenderArticle.bind(this)
+    this.deleteArticleAndRenderResponse = this.deleteArticleAndRenderResponse.bind(this)
+    this.renderReviews = this.renderReviews.bind(this)
+  }
+
+  renderHome (req, res) {
+    res.render('home')
+  }
+
+  async renderSingleArticle (req, res) {
+    const id = req.params.id
+
+    try {
+      const article = await this.articlesDao.getById(id)
+
+      if (!article) {
+        res.status(404).render('404')
+        return
+      }
+
+      res.render('article', {
+        id,
+        title: article.title,
+        content: article.content,
+        nameuser: article.nameuser
+      })
+    } catch (error) {
+      console.log(error)
+      res.status(500).render('500')
+    }
+  }
+
+  renderArticleCreationForm (req, res) {
+    res.render('article-form')
+  }
+
+  async renderReviews (req, res) {
+    const articles = await this.articlesDao.getAll()
+    res.render('reviews', {
+      articles
+    })
+  }
+
+  async renderArticleUpdateForm (req, res) {
+    const id = req.params.id
+
+    try {
+      const article = await this.articlesDao.getById(id)
+
+      if (!article) {
+        res.status(404).render('404')
+        return
+      }
+
+      res.render('article-form', {
+        id,
+        title: article.title,
+        content: article.content,
+        nameuser: article.nameuser
+      })
+    } catch (error) {
+      console.log(error)
+      res.status(500).render('500')
+    }
+  }
+
+  async insertAndRenderArticle (req, res) {
+    const title = req.body.title
+    const content = req.body.content
+    const nameuser = req.body.nameuser
+
+    const article = { title, content, nameuser }
+
+    try {
+      const id = await this.articlesDao.create(article)
+
+      res.redirect(`/articles/${id}`)
+    } catch (error) {
+      console.log(error)
+      res.status(500).render('500')
+    }
+  }
+
+  async updateAndRenderArticle (req, res) {
+    const id = req.params.id
+    const title = req.body.title
+    const content = req.body.content
+    const nameuser = req.body.nameuser
+
+    try {
+      const article = { title, content, nameuser, id }
+
+      await this.articlesDao.update(article)
+
+      res.redirect(`/articles/${id}`)
+    } catch (error) {
+      console.log(error)
+      res.status(500).render('500')
+    }
+  }
+
+  async deleteArticleAndRenderResponse (req, res) {
+    const id = req.params.id
+
+    try {
+      const article = await this.articlesDao.getById(id)
+
+      if (!article) {
+        res.status(404).render('404')
+        return
+      }
+
+      await this.articlesDao.delete(id)
+
+      res.render('article-deleted', {
+        id,
+        title: article.title,
+        nameuser: article.nameuser
+      })
+    } catch (error) {
+      console.log(error)
+      res.status(500).render('500')
+    }
+  }
+}
+
+module.exports = ArticlesController
---
+++ src/models/dao/ArticlesDAO.js
@@ -0,0 +1,42 @@
+class ArticlesDAO {
+  constructor (dbClient) {
+    this.db = dbClient
+    this.getAll = this.getAll.bind(this)
+    this.getById = this.getById.bind(this)
+    this.create = this.create.bind(this)
+    this.update = this.update.bind(this)
+    this.delete = this.delete.bind(this)
+  }
+
+  async getAll () {
+    const response = await this.db.query('SELECT id, title, content, nameuser FROM articles order by id desc')
+    const rows = response[0]
+    return rows
+  }
+
+  async getById (id) {
+    const response = await this.db.query('SELECT id, title, content, nameuser FROM articles WHERE id = ?', [id])
+    const rows = response[0]
+    return rows[0]
+  }
+
+  async create (article) {
+    const response = await this.db.query('INSERT INTO articles (title, content, nameuser) VALUES (?, ?, ?)', [article.title, article.content, article.nameuser])
+    const result = response[0]
+    return result.insertId
+  }
+
+  async update (article) {
+    const response = await this.db.query('UPDATE articles SET content = ?, nameuser = ? WHERE id = ?', [article.content, article.nameuser, article.id])
+    const result = response[0]
+    return result
+  }
+
+  async delete (id) {
+    const response = await this.db.query('DELETE FROM articles WHERE id = ?', [id])
+    const result = response[0]
+    return result
+  }
+}
+
+module.exports = ArticlesDAO
